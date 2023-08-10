313453c12fe7cc2304a6de4630a6b8296130b6d3
Nikita <nikycherenko@gmail.com>
2023-08-10 14:55:58 -0600

    connected Auth token with localStorage

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -8,22 +8,27 @@ const Auth = ({ colors }) => {
   const RESPONSE_TYPE = "token";
 
   const [token, setToken] = useState("");
+  console.log("token", token);
 
   useEffect(() => {
     const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
+    let storeToken = window.localStorage.getItem("token");
 
-    if (!token && hash) {
-      token = hash
+    if (!storeToken && hash) {
+      storeToken = hash
         .substring(1)
         .split("&")
         .find((elem) => elem.startsWith("access_token"))
         .split("=")[1];
       window.location.hash = "";
-      window.localStorage.setItem("token", token);
-      setToken(token);
+      window.localStorage.setItem("token", storeToken);
+      setToken(storeToken);
     }
-  }, []);
+
+    if (!token && storeToken) {
+      setToken(storeToken);
+    }
+  }, [token]);
 
   const logoutHandler = () => {
     setToken("");
